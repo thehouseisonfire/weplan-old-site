37e34f77743a1aeb1361188e736230d8d859966b
Juliano <desenvolvimento@weplan.com.br>
2023-06-21 15:58:41 -0300

    Improved the modal height

--- components/ModalFormProfissionalInteriores.vue
+++ components/ModalFormProfissionalInteriores.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <b-modal id="modal-form-profissional-interiores" ref="modalFormProfissionalInteriores" hide-header hide-footer @hidden="onHiddenModal">
-      <iframe width='100%' height='580'
+    <b-modal id="modal-form-profissional-interiores" ref="modalFormProfissionalInteriores" hide-footer @hidden="onHiddenModal">
+      <iframe
+        style="width: 100%; height: 100vh;"
         src='https://lp.weplan.com.br/form-seja-parceiro'
         frameborder='0'
       ></iframe>
@@ -21,5 +22,15 @@ export default {
   color: #fff;
   box-shadow: 0 0 0 0.2rem #1c1c1c40;
   padding: 0;
+}
+
+.modal-header {
+  border-radius: 0;
+  background-color: #1c1c1c;
+  border: 1px solid #000;
+}
+
+.modal-header .close {
+  color: #fff;
 }
 </style>
