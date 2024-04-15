1282b5fabef823dd01bbba8bfe25dc7269b1190d
Your Name <you@example.com>
2024-04-15 08:58:36 -0300

    bump deps, turn on View Transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -163,24 +163,24 @@ export default defineNuxtConfig(
             }]
           ]
         },
-        // gtm: {
-        //   id: process.env.GTM_ID,
-        //   // queryParams: {
-        //   //   gtm_auth: 'AB7cDEf3GHIjkl-MnOP8qr',
-        //   //   gtm_preview: 'env-4',
-        //   //   gtm_cookies_win: 'x',
-        //   // },
-        //   defer: true,
-        //   compatibility: false, // IE stuff?
-        //   nonce: '2726c7f26c',
-        //   enabled: false,
-        //   debug: false,
-        //   loadScript: true,
-        //   enableRouterSync: true,
-        //   ignoredViews: [],
-        //   trackOnNextTick: false,
-        //   devtools: false,
-        // },
+        gtm: {
+          id: process.env.GTM_ID,
+          // queryParams: {
+          //   gtm_auth: 'AB7cDEf3GHIjkl-MnOP8qr',
+          //   gtm_preview: 'env-4',
+          //   gtm_cookies_win: 'x',
+          // },
+          defer: true,
+          compatibility: false, // IE stuff?
+          nonce: '2726c7f26c',
+          enabled: false,
+          debug: false,
+          loadScript: true,
+          enableRouterSync: true,
+          ignoredViews: [],
+          trackOnNextTick: false,
+          devtools: false,
+        },
         facebook: {
           pixelId: process.env.PIXEL_ID,
           autoPageView: true,
@@ -197,7 +197,7 @@ export default defineNuxtConfig(
       '@nuxtjs/google-fonts',
       'nuxt3-meta-pixel',
       'nuxt-gtag',
-      // '@zadigetvoltaire/nuxt-gtm',
+      '@zadigetvoltaire/nuxt-gtm',
       'nuxt-clarity-analytics',
     ],
 
